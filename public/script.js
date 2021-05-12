4afbf949ada055fb6fbfad80bdb23db2bf213d4d
Cecilia Cai <ceciliacai@10-209-82-120.dynapool.shanghai.nyu.edu>
2021-05-12 19:19:36 +0800

    UI update

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 let socket;
-// socket = io.connect("http://localhost:5000");
-socket = io.connect(location.origin.replace(/^http/, "ws"));
+socket = io.connect("http://localhost:5000");
+// socket = io.connect(location.origin.replace(/^http/, "ws"));
 
 let sktID;
 socket.on("connect", function () {
